9034f68cf72d2119feaf7aef0d44190f56430ce0
1MuRaD12 <tu71tfsws@code.edu.az>
2022-06-08 10:48:56 +0300

    first commit

---
+++ New folder/AP204_Pronia/ViewModels/HomeVB.cs
@@ -0,0 +1,17 @@
+﻿using AP204_Pronia.Models;
+using AP204_Pronia.Services;
+using System.Collections.Generic;
+
+namespace AP204_Pronia.ViewModels
+{
+    public class HomeVB
+    {
+        public List<Slider> Sliders { get; set; }
+
+        public List<Plant> Plants { get; set; }
+
+
+        public List<Setting> settings { get; set; }
+
+    }
+}
